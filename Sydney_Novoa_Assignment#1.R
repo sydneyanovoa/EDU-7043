03a2421a15a197921398ff321f3f3f6fe0c1a2ba
sydneyanovoa <42783432+sydneyanovoa@users.noreply.github.com>
2018-09-05 22:39:11 -0500

    Add files via upload

---
+++ Sydney_Novoa_Assignment#1.R
@@ -0,0 +1,31 @@
+#Create the vector x,y,z
+x<-c(5,10,15,20,25,30)
+y<-c(-1,NA, 75,3,5,8)
+z<-c(5)
+#Multiply first two vectors by z
+x*z
+y*z
+#Store new objects
+Object1<-(x*z)
+Object2<-(y*z)
+#Print vectors
+print(Object1)
+print(Object2)
+#Replacing missing value
+y<-ifelse(is.na(y), 2.5, y)
+#Load dataset
+library(readr)prb<-read_csv(file = "https://raw.githubusercontent.com/mattdemography/EDU_7043/master/Data/Assignment_1.csv" )
+#Print state abbreviations
+head (state.abb,n=10)
+#Mean of murder
+mean (Assignment_1$Murder)
+#Median of murder
+median (Assignment_1$Murder)
+#Mean of NE murder 
+NewEngland<-subset(Assignment_1, State== "CT"|State== "NH"|State== "ME"|State=="MA"|State== "RI"|State=="VT")
+mean(NewEngland$Murder)
+#Bonus
+mean(Assignment_1$Vcrime, na.rm= TRUE)
+write.csv(Novoa_Sydney_Assignment_#1.docx, Novoa_Sydney_Assignment_#1.csv")
+write.csv(NewEngland,"~/Destop/NewEngland.csv")
+          
